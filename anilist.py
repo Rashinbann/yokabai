e7811b2a8cec483566509dfb76118953768843eb
pancaek <20342389+pancaek@users.noreply.github.com>
2024-05-05 12:21:36 -0700

    use match-case instead of if for chapters/volumes etc

--- anilist.py
+++ anilist.py
@@ -30,16 +30,16 @@ async def manga(ctx, *name):
     embed.set_image(url=anime_dict["banner_image"])
     chapters = anime_dict['chapters']
     volumes = anime_dict['volumes']
-    info = ""
-    if chapters is None and volumes is None:
-        info = "Info unavailable"
-    else:
-        if chapters:
-            info += f"Chapters: {chapters}"
-        if volumes:
-            if chapters:
-                info += "\n"
-            info += f"Volumes: {volumes}"
+    match (chapters, volumes):
+        case (None, None):
+            info = "Info unavailable"
+        case (chapters, None):
+            info = f"Chapters: {chapters}"
+        case (None, volumes):
+            info = f"Volumes: {volumes}"
+        case (chapters, volumes):
+            info = f"Chapters: {chapters}\nVolumes: {volumes}"
+
     descdict = anime_dict['desc']
     replace_descdict = {"<br>": "\n", "<i>": "*", "</i>": "*"}
     for old, new in replace_descdict.items():
@@ -66,22 +66,21 @@ async def anime(ctx, *name):
     embed.set_footer(text=anime_dict['genres'])
     embed.set_thumbnail(url=anime_dict['cover_image'])
     embed.set_image(url=anime_dict['banner_image'])
-    # TO DO make it so that it doesn't actually print chapters/volumes if that info doesn't exist.
-    # Right now it just gives None
-    info = f"Episoodes: {anime_dict['airing_episodes']} \nSeason: {anime_dict['season']}"
+
     episodes = anime_dict['airing_episodes']
     season = anime_dict['season'].capitalize()
 
     info = ""
-    if episodes is None and season is None:
-        info = "Info unavailable"
-    else:
-        if episodes:
-            info += f"Episodes: {episodes}"
-        if season:
-            if episodes:
-                info += "\n"
-            info += f"Season: {season}"
+    match (episodes, season):
+        case (None, None):
+            info = "Info unavailable"
+        case (episodes, None):
+            info = f"Episodes: {episodes}"
+        case (None, season):
+            info = f"Season: {season}"
+        case (episodes, season):
+            info = f"Episodes: {episodes}\nSeason: {season}"
+
     descdict = anime_dict['desc']
     replace_descdict = {"<br>": "\n", "<i>": "*", "</i>": "*"}
     for old, new in replace_descdict.items():
