4a20791358ef9f05189d6034692d75f89e55312d
rei <141047692+Rashinbann@users.noreply.github.com>
2024-05-21 08:14:24 +0300

    Made an embed for user info and added some info to it...

--- user.py
+++ user.py
@@ -3,17 +3,27 @@ from discord.ext import commands
 from bs4 import BeautifulSoup
 import requests
 
+
 @commands.command()
 async def user(ctx, *name):
     user = ctx.message.content.removeprefix(".user ")
     page = requests.get(f"https://anilist.co/user/{user}")
     soup = BeautifulSoup(page.text, features="html.parser")
-    username = soup.find('h1', attrs={'class':'name'}).string
-    avatar = soup.findAll('img', attrs={'class':'avatar'})
+    username = soup.find('h1', attrs={'class': 'name'}).string
+    avatar = soup.findAll('img', attrs={'class': 'avatar'})
     link = avatar[0]
     getlink = link.get('src')
-    about = soup.find('div', attrs={'class':'about'}).string
-    await ctx.send(f"{username}\n{about} {getlink} ")
+    about = soup.find('div', attrs={'class': 'about'}).string
+    totalAni = soup.find('div', attrs={'class': 'value'}).string
+    embed = discord.Embed(
+        title=username
+    )
+    embed.set_thumbnail(url=getlink)
+    embed.set_author(name='Go to page',
+                     url=f"https://anilist.co/user/{user}")
+    embed.insert_field_at(0, name='About', value=about)
+    embed.insert_field_at(1, name='Info', value=totalAni)
+    await ctx.send(embed=embed)
 
 
 async def setup(bot):
