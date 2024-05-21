2a8bd12088412c50fc30f3b27c1db5eab6c08c25
rei <141047692+Rashinbann@users.noreply.github.com>
2024-05-21 11:10:46 +0300

    Added relative date info. And stuff in user.py

--- user.py
+++ user.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from bs4 import BeautifulSoup
 import requests
 
+def pretty_list(l, sep='\n'):
+    return sep.join(l)
 
 @commands.command()
 async def user(ctx, *name):
@@ -10,23 +12,20 @@ async def user(ctx, *name):
     page = requests.get(f"https://anilist.co/user/{user}")
     soup = BeautifulSoup(page.text, features="html.parser")
     username = soup.find('h1', attrs={'class': 'name'}).string
-    avatar = soup.findAll('img', attrs={'class': 'avatar'})
-    link = avatar[0].get('src')
-    about = soup.find('div', attrs={'class': 'about'}).string
+    avatar = soup.findAll('img', attrs={'class': 'avatar'})[0].get('src')
+    about = soup.find('div', attrs={'class': 'about'}, recursive=True).get_text()
+    print(about)
     stats = soup.findAll('div', attrs={'class': 'stat'})
-
-    for stat in stats:
-        value=stat.findChildren('div', attrs={'class': 'value'}, recursive=False).string
-        print(value)
-
+    data = [stat.findChildren('div', attrs={'class': 'value'}, recursive=False)[0].string for stat in stats]
     embed = discord.Embed(
         title=username
     )
-    embed.set_thumbnail(url=getlink)
+    embed.set_thumbnail(url=avatar)
     embed.set_author(name='Go to page',
                      url=f"https://anilist.co/user/{user}")
     embed.insert_field_at(0, name='About', value=about)
-    embed.insert_field_at(1, name='Info', value=totalAni)
+    gay = [f'Total anime: {data[0]}', f'Days watched: {data[1]}', f'Mean score: {data[2]}']
+    embed.insert_field_at(1, name='Info', value=pretty_list(gay))
     await ctx.send(embed=embed)
 
 
