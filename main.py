bf613e54b99a53e5bb3f8480b0114b58a72de714
rei <141047692+Rashinbann@users.noreply.github.com>
2024-05-24 15:30:07 +0300

    added activity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext import tasks, commands
-from discord import Intents, Message
 import sqlite3
-import requests
 from dotenv import dotenv_values
 import os
 
@@ -11,10 +8,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+activity = discord.Activity(name="yokabai", type=discord.ActivityType.competing)
+bot = commands.Bot(command_prefix=".", intents=intents, activity=activity)
 
-bot = commands.Bot(command_prefix='.', intents=intents)
-
-db = sqlite3.connect('streakdb.db')
+db = sqlite3.connect("streakdb.db")
 cursor = db.cursor()
 
 
@@ -25,6 +22,7 @@ async def on_ready():
     await bot.load_extension("anilist")
     await bot.load_extension("user")
     await bot.load_extension("test")
+
 
 TOKEN = dotenv_values(".env").get("YOKABAI_TOKEN") or ""
 bot.run(TOKEN)
