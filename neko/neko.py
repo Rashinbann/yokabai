edf3e4fb67a3ad3fd3871e986198503532fdb4cd
rei <141047692+Rashinbann@users.noreply.github.com>
2024-05-26 13:05:22 +0300

    Added images to button interaction embeds

--- neko/neko.py
+++ neko/neko.py
@@ -5,6 +5,7 @@ import random
 cats = ["Siamese", "Persian", "Sphinx"]
 catimg = "https://cdn.discordapp.com/avatars/993804277631963206/3071341d6a3437a0572cf11d8a11f1c9.png?size=256?size=128"
 catimgsad = "https://cdn.discordapp.com/attachments/1044500978973540374/1244213873955962930/tuCu6BT-_400x400.jpg?ex=66544bf7&is=6652fa77&hm=7ed639f6df0c2068c0576e2426371ef2ad02ae6c27511c399ea96132826d02ce&"
+catimghappy = "https://cdn.discordapp.com/attachments/1044500978973540374/1244229154203897856/web_cat.jpg?ex=66545a33&is=665308b3&hm=032ecba7d6c53c9287a00079bbbc228ada32ba2b8859cddd83b7c885a258a2e4&"
 
 
 class ViewDesu(discord.ui.View):
@@ -12,19 +13,19 @@ class ViewDesu(discord.ui.View):
     foo: bool = None
 
     async def disable_all_items(self):
-        timeoutembed = discord.Embed(title="Out of time! The cat left")
+        for item in self.children:
+            item.disabled = True
+
+    async def on_timeout(self) -> None:
+        timeoutembed = discord.Embed(title="Out of time! The cat left...")
         timeoutembed.insert_field_at(
             0,
             name="You have to do something or the cat will get bored!",
             value="You can feed the cat to befriend it or ignore the cat... if you'd like :pouting_cat:",
         )
         timeoutembed.set_thumbnail(url=catimg)
-        for item in self.children:
-            item.disabled = True
-        await self.message.edit(embed=timeoutembed, view=self)
-
-    async def on_timeout(self) -> None:
         await self.disable_all_items()
+        await self.message.edit(embed=timeoutembed, view=self)
 
     @discord.ui.button(label="Feed", style=discord.ButtonStyle.success)
     async def Feed(self, interaction: discord.Interaction, button: discord.ui.button):
@@ -36,13 +37,15 @@ class ViewDesu(discord.ui.View):
                 name="You're my frind now!",
                 value="The cat is now your friend! You can have soft tacos later!",
             )
+            embedfed.set_thumbnail(url=catimghappy)
         else:
             embedfed.insert_field_at(
                 0,
                 name="The cat took the food and ran off into the distance....",
                 value="Better luck next time!",
             )
-        await interaction.response.edit_message(embed=embedfed)
+            embedfed.set_thumbnail(url=catimghappy)
+        await interaction.response.edit_message(embed=embedfed, view=None)
         self.foo = True
         self.stop()
 
@@ -72,6 +75,7 @@ async def neko(ctx):
     message = await ctx.send(embed=embed, view=view)
     view.message = message
     await view.wait()
+    await view.disable_all_items()
 
     if view.foo is None:
         print("Timeout")
